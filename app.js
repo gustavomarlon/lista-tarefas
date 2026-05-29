6b54fa04a2b6091a7e495d6480f128fc404f3267
felipe9436-cpu <felipe.9436@colegioprogresso.g12.br>
2026-05-29 07:11:34 -0300

    feito

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const input = document.getElementById('input')
-const button = document.getElementById('button')
-const display = document.getElementById('display')
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const display = document.getElementById("display");
 
 function inserirTarefa() {
-    let tarefa = input.value
+    let tarefa = input.value;
+
+    if (tarefa == '') {
+        alert('Favor inserir uma tarefa')
+        return
+    }
+
     display.innerHTML +=
-        `<div class = "tarefa">
-        ${tarefa}
-        <div>
-            <button class="delete">deletar</button>
-            <button class= "edit"> editar </button>
-        </div>
-    </div>`
+        `<div class="tarefa">
+      ${tarefa}
+         <div>
+            <button class="delete"> Deletar </button>
+            <button class="edit"> Editar </button>
+         <div>
+    </div>`;
     input.value = ''
 }
+
+function removerTarefa(event) {
+    if (event.target.classList.contains('delete')) {
+        event.target.parentElement.parentElement.remove()
+    }
+}
+
 button.addEventListener("click", inserirTarefa)
+display.addEventListener("click", removerTarefa)
